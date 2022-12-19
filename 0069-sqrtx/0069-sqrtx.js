dae9e3f03a57f49be09ce3e40000817aeef9f309
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2022-12-19 16:07:58 -0300

    Time: 130 ms (41.91%), Space: 44.4 MB (9.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -3,5 +3,23 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x));
+    let left = 1;
+    
+    let right = Math.floor(x / 2 + 1);
+    
+    let mid = 0;
+    
+    while (left <= right) {
+        const mid = Math.round((left + right) / 2);
+        
+        if (mid * mid > x) {
+            right = mid - 1;
+        } else if (mid * mid < x) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    
+    return right;
 };
