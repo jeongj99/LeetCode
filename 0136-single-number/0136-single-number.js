dec23b13675fd1c4ad0d67c693860ed929d8a461
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-01-13 22:00:26 -0300

    Time: 77 ms (80.97%), Space: 47.3 MB (24.46%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
+    const hashTable = {};
+    
     for (const num of nums) {
-        if (nums.indexOf(num) === nums.lastIndexOf(num)) {
-            return num;
-        }
+        hashTable[num] = hashTable[num] + 1 || 1;
     }
+    
+    return Object.keys(hashTable).find(key => hashTable[key] === 1);
 };
