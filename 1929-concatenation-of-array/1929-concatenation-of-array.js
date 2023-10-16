f3e5a9e512e5bebe634542d720fd885e99133537
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-10-16 16:29:10 -0400

    Time: 78 ms (5.37%), Space: 45.9 MB (35.74%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -3,12 +3,10 @@
  * @return {number[]}
  */
 var getConcatenation = function(nums) {
-    const ans = [];
+    const ans = [...nums];
     
-    for (let n = 1; n <= 2; n++) {
-        for (let i = 0; i < nums.length; i++) {
-            ans.push(nums[i]);
-        }
+    for (let i = 0; i < nums.length; i++) {
+        ans.push(nums[i]);
     }
     
     return ans;
