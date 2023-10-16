00a579005954ebe1119635ca48ffc44dad9464da
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-10-16 13:59:23 -0400

    Time: 115 ms (19.48%), Space: 44.7 MB (48.15%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -4,10 +4,10 @@
  */
 var removeDuplicates = function(nums) {
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === nums[i + 1]) {
-            nums.splice(i, 1);
-            i--;
-        }
+      if (nums[i] === nums[i + 1]) {
+          nums.splice(i, 1)
+          i--;
+      }  
     }
-    return nums.length;
+    return nums.length
 };
