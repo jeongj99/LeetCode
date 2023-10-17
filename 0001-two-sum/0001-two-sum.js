75004434362b8b5bb81662b82ffbe4990af08b88
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-10-17 15:28:16 -0400

    Time: 100 ms (23.47%), Space: 44 MB (14.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,13 +4,20 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+    const seenNumbers = [];
     const result = [];
+    
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                result.push(i, j)
-            }
+        if (seenNumbers.includes(target - nums[i])) {
+            const j = nums.findIndex((element) => {
+                return element === (target - nums[i]);
+            })
+            result.push(j, i);
+            return result;
         }
+        
+        seenNumbers.push(nums[i]);
     }
+    
     return result;
 };
