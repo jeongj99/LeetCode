f08734be66d51bd917897d349431b38fcf2fc7d4
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-10-17 00:17:08 -0400

    Time: 145 ms (23.45%), Space: 52 MB (82.61%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -3,11 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+    nums.sort((a, b) => {
+        return a - b;
+    });
+    
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] === nums[j]) {
-                return true;
-            }
+        if (nums[i] === nums[i + 1]) {
+            return true;
         }
     }
     
