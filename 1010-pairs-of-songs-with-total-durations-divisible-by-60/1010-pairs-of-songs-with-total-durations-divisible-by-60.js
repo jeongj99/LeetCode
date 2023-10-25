2ab3e38c4e2b24402f8b87c6a733de87b14b28da
Jae Yun Jeong <109401369+jeongj99@users.noreply.github.com>
2023-10-25 15:32:27 -0400

    Time: 57 ms (82.14%), Space: 46 MB (39.29%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var numPairsDivisibleBy60 = function(time) {
-    let remainderCount = Array(60).fill(0);
+    let remainderCount = {};
     let count = 0;
 
     for (let t of time) {
         let remainder = t % 60;
         count += remainderCount[(60 - remainder) % 60] || 0;
-        remainderCount[remainder]++;
+        remainderCount[remainder] = (remainderCount[remainder] || 0) + 1;
     }
 
     return count;
